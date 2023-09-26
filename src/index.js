9b7b7a7e2114953ef4caa74747d579b598017600
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-09-25 23:03:56 -0500

    added some starter Html stuff & some code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-fetch("")
+//just a const list that may come in handy later
+const Data = ((r)=> r.json())
+const showsURL = "http://localhost:3000/tvshows"
+const showsDropDown = document.getElementById(`collection`)
+//rendering a show to html
+const renderShows = (show) => {
+    console.log(show)
+    const div = document.createElement(`div`)
+    div.innerText = show.title
+    showsDropDown.append(div)
+}
+//fetching from DB then for eaching them
+fetch(showsURL)
+.then(Data)
+.then((showsArray)=>{
+    showsArray.forEach(renderShows)
+})
