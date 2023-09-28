3cf4f74b3348e3dd6e1bf7b9d33a57247b56fbbb
Kassidy Puello <kassidympuello@gmail.com>
2023-09-27 21:50:17 -0400

    a whole lot of messing around

--- src/index.js
+++ src/index.js
@@ -5,34 +5,98 @@ const showsDropDown = document.getElementById(`collection`)
 const placeholderImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbMI_lmqrrCTrEZkZym0_XjHkDn4eqDomJsQ&usqp=CAU"
 
 //fetching from DB then for eaching them
-fetch(showsURL)
-.then(Data)
-.then((showsArray)=>{
-    showsArray.forEach(renderShows)
-})
+// fetch(showsURL)
+// .then(Data)
+// .then((showsArray)=>{
+//     showsArray.forEach(renderShows)
+// })
+
+// Rendering a show to HTML
+const renderShows = (show) => {
+    const singleShow = document.createElement(`div`);
+    singleShow.innerText = show.title;
+    singleShow.className = `show`;
+    singleShow.draggable = true;
+  
+    singleShow.addEventListener("click", () => {
+      document.getElementById("details-title").innerText = show.title;
+      document.getElementById("details-genre").innerText = show.genre;
+      document.getElementById("details-img").src = show.image;
+      document.getElementById("details-img").alt = show.title + " poster";
+    });
+    
+    singleShow.addEventListener("dragstart", (e) => {
+        e.dataTransfer.setData("text/plain", e.target.id);
+      });
+  
+    singleShow.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+  
+    singleShow.addEventListener("dragenter", (e) => {
+      e.target.classList.add("drag-over");
+    });
+  
+    singleShow.addEventListener("dragleave", (e) => {
+      e.target.classList.remove("drag-over");
+    });
+  
+    singleShow.addEventListener("drop", (e) => {
+      e.preventDefault();
+      const itemId = e.dataTransfer.getData("text/plain");
+      const item = document.getElementById(itemId);
+      e.target.parentElement.insertBefore(item, e.target.nextSibling);
+      e.target.classList.remove("drag-over");
+    });
+  
+    showsDropDown.append(singleShow);
+  };
+  
+  // Fetching shows data and rendering them
+  fetch(showsURL)
+    .then(response => response.json())
+    .then(showsArray => {
+      showsArray.forEach(renderShows);
+    })
+    .catch(error => console.error(error));
 
 //rendering a show to html
-const renderShows = (show) => {
-    const singleShow = document.createElement(`div`)
-    singleShow.innerText = show.title
-    singleShow.className = `show`
-    singleShow.addEventListener("click", () => {
-        document.getElementById("details-title").innerText = show.title
-        document.getElementById("details-genre").innerText = show.genre
-        document.getElementById("details-img").src = show.image
-        document.getElementById("details-img").alt = show.title + " poster"
-    })
-    showsDropDown.append(singleShow)
+// const renderShows = (show) => {
+//     const singleShow = document.createElement(`div`)
+//     singleShow.innerText = show.title
+//     singleShow.className = `draggable`
+//     singleShow.draggable = true
+//     singleShow.addEventListener("click", () => {
+//         document.getElementById("details-title").innerText = show.title
+//         document.getElementById("details-genre").innerText = show.genre
+//         document.getElementById("details-img").src = show.image
+//         document.getElementById("details-img").alt = show.title + " poster"
+//     })
+//     showsDropDown.append(singleShow)
 
-    singleShow.addEventListener('mouseover',(event) => {
-        console.log(event)
-        event.target.style.color = "orange";
-        setTimeout(() => {
-            event.target.style.color = "";
-          }, 1000);
-    })
-    document.getElementById("details-img").src = placeholderImage;
-}
+//     singleShow.addEventListener('mouseover',(event) => {
+//         console.log(event)
+//         event.target.style.color = "orange";
+//         setTimeout(() => {
+//             event.target.style.color = "";
+//           }, 1000);
+//     })
+    
+// document.getElementById("details-img").src = placeholderImage;
+
+// const draggables = document.querySelectorAll('.draggable')
+// // const dragContainer = document.querySelectorAll('.dragContainer')
+
+// draggables.forEach(draggable => {
+//   draggable.addEventListener('dragstart', () => {
+//     draggable.classList.add('dragging')
+//   })
+
+//   draggable.addEventListener('dragend', () => {
+//     draggable.classList.remove('dragging')
+//   })
+// })
+// }
 
 // created form function that adds new tv show user inputs into the list however it does not presist yet -kp
 const form = document.querySelector(".add-show-form")
@@ -66,3 +130,10 @@ form.addEventListener('submit',(event)=>{
     .catch(( error ) => console.log(error))
 })
 
+// const heartButton = document.querySelector( 'heart' )
+// const likesContainer = document.querySelector( '.number-of-likes' )
+// heartButton.addEventListener('click',(event)=>{
+//     // when user clicks button number of likes will increase
+//     // when user click different tv show the number of likes reset
+//     // possibly save number of likes when user clicks back on image
+// })
